aede31008d97724761d04f4726b97f5789486e53
Steve Dunn <steve@dunnhq.com>
2022-11-05 07:53:54 +0000

    First commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿var scenarioTypes = typeof(Program).Assembly.GetTypes().Where(t => typeof(IScenario).IsAssignableFrom(t) && t != typeof(IScenario)).ToList();
+
+foreach (var eachScenarioType in scenarioTypes)
+{
+    WriteBanner(eachScenarioType);
+    var instance = (IScenario)Activator.CreateInstance(eachScenarioType)!;
+    
+    await instance.Run();
+}
+
+Console.WriteLine("Finished");
+
+
+static void WriteBanner(Type eachScenarioType)
+{
+    Console.WriteLine();
+    Console.WriteLine();
+    Console.WriteLine();
+    Console.WriteLine("==========================================================");
+    Console.WriteLine($"Running {eachScenarioType.Name}");
+}
+
+public interface IScenario
+{
+    Task Run();
+}
---
+++ PubSubScenario.cs
@@ -0,0 +1,51 @@
+﻿using StackExchange.Redis;
+using System.Threading.Channels;
+
+namespace RedisTestbench;
+
+
+public class PubSubScenario : IScenario
+{
+    static readonly ConnectionMultiplexer redis = ConnectionMultiplexer.Connect(
+        new ConfigurationOptions
+        {
+            EndPoints = { "localhost:6379" }
+        });
+    
+    public async Task Run()
+    {
+        var db = redis.GetDatabase();
+
+        await db.StringSetAsync("foo:bar", "hello world!");
+
+        string? s = await db.StringGetAsync("foo:bar");
+
+        var pubsub = redis.GetSubscriber();
+
+        await Task.WhenAll(new[]
+        {
+            RunPub(pubsub),
+            RunSub(pubsub)
+        });
+
+        await pubsub.PublishAsync("test-channel", "This is a test message!!", CommandFlags.FireAndForget);
+        Console.Write("Message Successfully sent to test-channel");
+
+        Console.WriteLine(s);
+    }
+
+    private async Task RunPub(ISubscriber subscriber)
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            await subscriber.PublishAsync("test-channel", $"message {i}");
+
+            await Task.Delay(500);
+        }
+    }
+
+    private async Task RunSub(ISubscriber subscriber)
+    {
+        await subscriber.SubscribeAsync("test-channel", (channel, message) => Console.WriteLine($"Received {message} from {channel}"));
+    }
+}
